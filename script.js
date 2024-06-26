9eccd41f57fbb8305b3ee26492b8607f94220e76
Vincent <121472782+MosquitoOM@users.noreply.github.com>
2024-06-26 10:28:19 +0800

    script.js

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ function toggleChat() {
     if (chatBox.style.display === 'none' || chatBox.style.display === '') {
         chatBox.style.display = 'flex';
         iconImg.src = 'close-icon.png'; // 替換成叉叉的圖案
-        iconImg.style.width = '60px'; // 保持與原客服機器人圖示一致
-        iconImg.style.height = '60px'; // 保持與原客服機器人圖示一致
     } else {
         chatBox.style.display = 'none';
         iconImg.src = 'chatbot-icon.png'; // 替換回智能客服機器人的圖案
-        iconImg.style.width = '60px'; // 保持與原客服機器人圖示一致
-        iconImg.style.height = '60px'; // 保持與原客服機器人圖示一致
     }
 }
 
